f824e5f5a19cfeafd8ca9f4da2bd245631d911ee
darkoverlord-design <darkoverlord-design@users.noreply.github.com>
2026-07-27 13:14:30 -0700

    Use public frontend URL for backend redirect

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 
-FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://127.0.0.1:5173/')
+FRONTEND_URL = os.getenv('FRONTEND_URL', 'https://couriertracker.vercel.app/')
 
 urlpatterns = [
     path('', RedirectView.as_view(url=FRONTEND_URL, permanent=False), name='home'),
